888d6151703ec25f28887323eaeb40317cdaa25a
veronika.borup <veronika.borup.syne17@tucsweden.se>
2018-04-04 16:00:32 +0200

    Added a view for task 1 - moviecollection in database.sql and a query for task 1 in queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 -- Fråga 1: Vilka filmer som firman äger, inklusive data om filmen.
 
-SELECT * FROM products;
+SELECT * FROM movie_collection;
 
 -- Fråga 2: Vilka filmer som finns i en viss genre.
 
